ccb8cbadf5bac01ed953e5078c54ef25e5a3de5b
Per Nordlow <per.nordlow@autoliv.com>
2017-05-08 09:44:21 +0200

    Better code

--- containerize.py
+++ containerize.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 
+# Write file to calls/xx/yy/xxyy... .txt with contents FILENAME MTIME HASH
+
 # Only write outputs if newer or content different
 
 # TODO 1. Add wrapper for subprocess.popen
@@ -38,7 +40,9 @@ _SUCCESS = 0                    # default success exit status
 _FAILURE = 1                    # default failure exit status
 
 _DEFAULT_HASH_NAME = 'sha256'  # either md5, sha1, sha256, sha512, etc
-_DEFAULT_CACHE_DIR = os.path.join(os.path.expanduser('~'), '.cache', __name__)
+
+_HOME_DIR = os.path.expanduser('~')
+_DEFAULT_CACHE_DIR = os.path.join(_HOME_DIR, '.cache', __name__)
 
 
 # Input file (regular or directory) path.
@@ -107,19 +111,19 @@ def _hash_update_data(hash_, data):
 def _atomic_copyfile(src, dst, overwrite, logger):
     try:
         with tempfile.NamedTemporaryFile(dir=os.path.dirname(dst),
-                                         delete=False) as tmp_h:
+                                         delete=False) as tmp_handle:
             with open(src, 'rb') as src_fd:
                 shutil.copyfileobj(fsrc=src_fd,
-                                   fdst=tmp_h)
+                                   fdst=tmp_handle)
         if overwrite:
             # works both on Windows and Linux from Python 3.3+, os.rename raises an
             # exception on Windows if the file exists
-            os.replace(src=tmp_h.name,
+            os.replace(src=tmp_handle.name,
                        dst=dst)
             return True
         else:
             if not os.path.exists(dst):
-                os.rename(src=tmp_h.name,
+                os.rename(src=tmp_handle.name,
                           dst=dst)
                 return True
     except:
@@ -127,7 +131,7 @@ def _atomic_copyfile(src, dst, overwrite, logger):
         pass
     finally:
         try:
-            os.remove(tmp_h.name)
+            os.remove(tmp_handle.name)
         except:
             pass
     return False
@@ -135,10 +139,10 @@ def _atomic_copyfile(src, dst, overwrite, logger):
 
 def _file_hexdigest(file_name,
                     hash_name):
-    chash = hashlib.new(name=hash_name)
-    with open(file_name, 'rb') as out_h:
-        chash.update(out_h.read())
-    return chash.hexdigest()
+    hash_state = hashlib.new(name=hash_name)
+    with open(file_name, 'rb') as out_handle:
+        hash_state.update(out_handle.read())
+    return hash_state.hexdigest()
 
 
 def _try_store_into_cache(out_files,
@@ -147,16 +151,14 @@ def _try_store_into_cache(out_files,
                           hash_name,
                           logger):
     try:
-        with open(cache_manifest_file, 'w') as manifest_h:
+        with open(cache_manifest_file, 'w') as manifest_handle:
             for out_file in out_files:
                 assert not out_file.is_absolute()
                 out_file_name = str(out_file)  # just the name
 
                 hexdig = _file_hexdigest(file_name=out_file_name,
                                          hash_name=hash_name)
-
-                cache_artifact_file = os.path.join(cache_artifacts_dir,
-                                                   hexdig)  # TODO use + os.path.splitext(out_file_name)[1]
+                cache_artifact_file = os.path.join(cache_artifacts_dir, hexdig)
 
                 # must not use link here
                 if _atomic_copyfile(src=out_file_name,
@@ -168,7 +170,7 @@ def _try_store_into_cache(out_files,
                     logger.info('Skipped storing {} with contents {} already in cache'.format(out_file_name, hexdig))
 
                 # write entry in manifest file
-                manifest_h.write(hexdig + ' ' + out_file_name + '\n')
+                manifest_handle.write(hexdig + ' ' + out_file_name + '\n')
 
         return True
     except FileNotFoundError as exc:
@@ -215,10 +217,10 @@ def _atomic_link_or_copyfile(src, dst, logger):
 
 
 def copy_input_to_box(work_dir, in_files,
-                      box_in_dir_abspath,
+                      in_dir_abspath,
                       logger):
-    os.mkdir(box_in_dir_abspath)
-    os.chdir(box_in_dir_abspath)
+    os.mkdir(in_dir_abspath)
+    os.chdir(in_dir_abspath)
     for in_file in in_files:
         boxed_in_dir = os.path.dirname(in_file.as_boxed())
         if boxed_in_dir:    # only if in_file.as_boxed() lies in a subdir
@@ -240,12 +242,12 @@ def copy_output_from_box(out_files,
 
 
 def create_out_dirs(out_files,
-                    box_out_dir_abspath):
-    os.mkdir(box_out_dir_abspath)
+                    out_dir_abspath):
+    os.mkdir(out_dir_abspath)
     for out_file in out_files:
         if not out_file.is_absolute():
             out_file = str(out_file)
-            box_out_file = os.path.join(box_out_dir_abspath, out_file)
+            box_out_file = os.path.join(out_dir_abspath, out_file)
             os.makedirs(os.path.dirname(box_out_file), exist_ok=True)  # pre-create directory
 
 
@@ -281,7 +283,7 @@ def isolated_call(typed_args,
     if use_caching:
         logger_dir = cache_dir
     else:
-        logger_dir = os.path.join(os.path.expanduser('~'), '.' + __name__)
+        logger_dir = os.path.join(_HOME_DIR, '.' + __name__)
     os.makedirs(logger_dir, exist_ok=True)
 
     # logging
@@ -292,15 +294,14 @@ def isolated_call(typed_args,
     formatter = logging.Formatter('%(asctime)s %(levelname)s: %(message)s')
 
     # file log
-    ch = logging.FileHandler(os.path.join(logger_dir, 'all.log'))
-    ch.setLevel(logging.DEBUG)
-    ch.setFormatter(formatter)
+    log_file = logging.FileHandler(os.path.join(logger_dir, 'all.log'))
+    log_file.setLevel(logging.DEBUG)
+    log_file.setFormatter(formatter)
+    top_logger.addHandler(log_file)
 
-    top_logger.addHandler(ch)
+    load_from_cache = False  # for debugging purpose. TODO remove before deployment
 
-    load_from_cache = False # for debugging purpose
-
-    chash = hashlib.new(name=hash_name)
+    hash_state = hashlib.new(name=hash_name)
 
     in_files = set()
     out_files = set()
@@ -320,10 +321,6 @@ def isolated_call(typed_args,
         bytes: '',
     }
 
-    def _check_path(path):
-        if path.is_absolute():
-            top_logger.warning("convert absolute path {} to relative".format(path))
-
     # process typed arguments
     args = []                # expand args
     for typed_arg in typed_args:
@@ -332,22 +329,19 @@ def isolated_call(typed_args,
         arg = str(typed_arg)
 
         if use_caching:
-            chash.update(arg.encode('utf8'))  # file name
+            hash_state.update(arg.encode('utf8'))  # file name
 
         if isinstance(typed_arg, InFilePath):
-            _check_path(typed_arg)
             in_files.add(typed_arg)
             if use_caching:
-                chash.update(open(typed_arg.as_unboxed(), 'rb').read())  # file content
+                hash_state.update(open(typed_arg.as_unboxed(), 'rb').read())  # file content
         elif isinstance(typed_arg, OutFilePath):
-            _check_path(typed_arg)
             out_files.add(typed_arg)
         elif isinstance(typed_arg, TempDirPath):
-            _check_path(typed_arg)
             temp_dirs.add(typed_arg)
         elif isinstance(typed_arg, ExecFilePath):
             if use_caching:
-                chash.update(open(typed_arg.as_unboxed(), 'rb').read())  # file content
+                hash_state.update(open(typed_arg.as_unboxed(), 'rb').read())  # file content
             # allow absolute file paths here for now
         else:
             assert isinstance(typed_arg, str)
@@ -359,12 +353,12 @@ def isolated_call(typed_args,
         for extra_input in extra_inputs:
             if isinstance(extra_input, bytes):
                 if use_caching:
-                    chash.update(extra_input)
+                    hash_state.update(extra_input)
             elif isinstance(extra_input, InFilePath):
                 in_files.add(extra_input)
                 if use_caching:
-                    chash.update(extra_input.as_boxed().encode('utf8'))  # file named
-                    chash.update(open(extra_input.as_unboxed(), 'rb').read())  # file content
+                    hash_state.update(extra_input.as_boxed().encode('utf8'))  # file named
+                    hash_state.update(open(extra_input.as_unboxed(), 'rb').read())  # file content
             else:
                 raise Exception('Cannot handle extra_input {} of type {}'
                                 .format(extra_input,
@@ -382,11 +376,11 @@ def isolated_call(typed_args,
             value = str(typed_value)
             env[name] = value
 
-            chash.update(name.encode('utf8'))
-            chash.update(value.encode('utf8'))
+            hash_state.update(name.encode('utf8'))
+            hash_state.update(value.encode('utf8'))
 
     if use_caching:
-        hexdig = chash.hexdigest()
+        hexdig = hash_state.hexdigest()
 
         cache_prefix_dir = os.path.join(cache_dir,
                                         hexdig[0:2],
@@ -412,49 +406,49 @@ def isolated_call(typed_args,
 
     # within sandbox
     with tempfile.TemporaryDirectory() as box_dir:
-        box_in_dir_abspath = os.path.join(box_dir, in_subdir_name)
-        box_out_dir_abspath = os.path.join(box_dir, out_subdir_name)
-        box_temp_dir_abspath = os.path.join(box_dir, temp_subdir_name)
+        in_dir_abspath = os.path.join(box_dir, in_subdir_name)
+        out_dir_abspath = os.path.join(box_dir, out_subdir_name)
+        temp_dir_abspath = os.path.join(box_dir, temp_subdir_name)
 
         copy_input_to_box(work_dir=work_dir,
                           in_files=in_files,
-                          box_in_dir_abspath=box_in_dir_abspath,
+                          in_dir_abspath=in_dir_abspath,
                           logger=top_logger)
 
         # create output directories
         create_out_dirs(out_files=out_files,
-                        box_out_dir_abspath=box_out_dir_abspath)
+                        out_dir_abspath=out_dir_abspath)
 
         # create top directory for temporary box files
-        os.makedirs(box_temp_dir_abspath)
+        os.makedirs(temp_dir_abspath)
 
         # NOTE keeping these because it's very useful when debugging file structure in container:
         # import print_fs
         # print_fs.print_tree(box_dir)
 
         # call in containerized read-only input directory
-        os.chdir(box_in_dir_abspath)
-        os.chmod(box_in_dir_abspath,
+        os.chdir(in_dir_abspath)
+        os.chmod(in_dir_abspath,
                  stat.S_IREAD | stat.S_IXUSR)  # read-only and executable
         exit_status = call(args=args,
                            env=env,
                            stderr=subprocess.STDOUT,
                            shell=shell,
                            timeout=timeout)
-        os.chmod(box_in_dir_abspath,
+        os.chmod(in_dir_abspath,
                  stat.S_IREAD |
                  stat.S_IWRITE |  # make it writeable again so that it can be removed
                  stat.S_IXUSR)
 
         # handle result
         if exit_status == _SUCCESS:
-            os.chdir(box_out_dir_abspath)  # enter sandbox output
+            os.chdir(out_dir_abspath)  # enter sandbox output
 
             # TODO merge these three processing of out_files
 
             if strip_box_in_dir_prefix:
                 _strip_from_out_file_contents(out_files=out_files,
-                                              prefix=box_in_dir_abspath + os.sep)
+                                              prefix=in_dir_abspath + os.sep)
 
             if use_caching:
                 _try_store_into_cache(out_files=out_files,
