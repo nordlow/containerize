cf831fb1ffe85e31b89952e8f1baa55646adaa0c
Per Nordlöw <per.nordlow@gmail.com>
2017-05-16 01:15:45 +0200

    Fix

--- containerize.py
+++ containerize.py
@@ -265,7 +265,7 @@ def copy_input_to_box(work_dir, in_files,
                                  logger=logger)
 
 
-# TODO if possible use `rename()` directly otherwise do `copy()` and `remove()`, then further down assert that output is empty
+# TODO if possible use `rename()` directly otherwise do `copy()` and `remove()`, then further down assert that `out_dir_abspath` is empty
 def copy_output_from_box(out_files,
                          work_dir,
                          logger):
